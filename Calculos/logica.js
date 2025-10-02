2d28f189e354a803a43cea79d41ba21c5ffebb83
antonysantos9 <antony.g.santos9@aluno.senai.br>
2025-10-02 16:41:57 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -1,6 +1,6 @@
 function soma(){
-    var numero1 = document.getElementById("n1").valueAsNumber
-    var numero2 = document.getElementById("n2").valueAsNumber
+    var numero1 = parseFloat(document.getElementById("n1").value)
+    var numero2 = parseFloat(document.getElementById("n2").value)
     var resposta = document.getElementById("respSoma")
 
     resposta.textContent = numero1 + numero2
@@ -8,9 +8,25 @@ function soma(){
 }
 
 function sub(){
-     var numero1 = document.getElementById("subn1").valueAsNumber
-    var numero2 = document.getElementById("subn2").valueAsNumber
+     var num1 = parseFloat(document.getElementById("subn1").value)
+    var num2 = parseFloat(document.getElementById("subn2").value)
     var resposta = document.getElementById("respSub")
 
-    resposta.textContent = numero1 - numero2
+    resposta.textContent = num1 - num2
+}
+
+function mult(){
+     var number1 = parseFloat(document.getElementById("mult1").value)
+    var number2 = parseFloat(document.getElementById("mult2").value)
+    var resposta = document.getElementById("respMult")
+
+    resposta.textContent = number1 * number2
+}
+
+function div(){
+     var numb1 = parseFloat(document.getElementById("div1").value)
+    var numb2 = parseFloat(document.getElementById("div2").value)
+    var resposta = document.getElementById("respDiv")
+
+    resposta.textContent = numb1 / numb2
 }
